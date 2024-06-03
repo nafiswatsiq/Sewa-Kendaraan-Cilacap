0f2fb8619397a4d583b02143ce214e74911e3c64
nafiswatsiq <amrullohnafis@gmail.com>
2024-06-03 09:00:45 +0700

    Add project files.

---
+++ Sewa Kendaraan Cilacap/config/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sewa_Kendaraan_Cilacap.config
+{
+    abstract class Config
+    {
+        //public static string connectionString = "Server=localhost;Database=siperpus;Uid=root;Pwd=;";
+
+        //eksekusi Insert, Update, Delete
+        public abstract int eksekusiBukanQuery(string query);
+
+        //eksekusi Select
+        public abstract DataTable eksekusiQuery(string query);
+    }
+}
---
+++ Sewa Kendaraan Cilacap/config/Connection.cs
@@ -0,0 +1,92 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sewa_Kendaraan_Cilacap.config
+{
+    internal class Connection:Config
+    {
+        MySqlConnection _conn;
+        MySqlCommand _command;
+        MySqlDataAdapter _adapter;
+
+        string url = "server=localhost;port=3306;database=siperpus;uid=root;pwd=";
+
+        public Connection()
+        {
+            _conn = new MySqlConnection(url);
+            _command = new MySqlCommand();
+            _adapter = new MySqlDataAdapter();
+        }
+
+        void bukaKoneksi()
+        {
+            if (_conn.State == ConnectionState.Closed)
+            {
+                _conn.Open();
+            }
+        }
+
+        void tutupKoneksi()
+        {
+            if (_conn.State == ConnectionState.Open)
+            {
+                _conn.Close();
+            }
+        }
+
+        public override int eksekusiBukanQuery(string query)
+        {
+
+            int kembalian = -1;
+
+            try
+            {
+                bukaKoneksi();
+
+                _command.Connection = _conn;
+                _command.CommandText = query;
+
+                kembalian = _command.ExecuteNonQuery();
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } finally
+            {
+                tutupKoneksi();
+            }
+
+            return kembalian;
+        }
+
+        public override DataTable eksekusiQuery(string query)
+        {
+            DataTable kembalian = new DataTable();
+
+            try
+            {
+                bukaKoneksi();
+
+                _command.Connection = _conn;
+                _command.CommandText = query;
+                _adapter.SelectCommand = _command;
+                _adapter.Fill(kembalian);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                tutupKoneksi();
+            }
+
+            return kembalian;
+        }
+    }
+}
